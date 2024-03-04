826cf6d58975b41ab0ace4a472bed98baf700b20
sagar <sagarc2580@gmail.com>
2024-03-04 14:03:34 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import './App.css'
+
+function App() {
+  return (
+     <div className="editor">
+
+          {/* tweet settings */}
+          <div className=" tweet-setting">
+
+          </div>
+
+         {/* divider line*/}
+
+         <div className="divider">
+
+         </div>
+
+
+         {/* tweet editor*/}
+
+         <div className="tweet-editor">
+             <img src="	https://pbs.twimg.com/profile_images/1731437883557343232/o0sPOK5F_bigger.jpg" alt="image"/>
+         </div>
+     </div>
+  );
+}
+
+export default App;
